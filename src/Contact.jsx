bb72f9924722320e1cca44a53924c21e8cd113ff
Azsaria114 <susiazsaria@gmail.com>
2025-12-29 12:25:24 +0530

    Update contact page typography and social icons alignment: Montserrat semibold for heading, Open Sans semibold for descriptions, Open Sans bold for social label, icons aligned to right

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -73,9 +73,8 @@ function Contact() {
             <div className="contact-main__divider"></div>
             
             <div className="contact-main__social">
-              <div className="contact-main__social-inline">
-                <p className="contact-main__social-label body-opensans">Social media</p>
-                <div className="contact-main__social-icons">
+              <p className="contact-main__social-label body-opensans">Social media</p>
+              <div className="contact-main__social-icons">
                   <a href="#" className="contact-main__social-icon" aria-label="Facebook">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z" stroke="currentColor" strokeWidth="1.5" fill="none"/>
@@ -100,7 +99,6 @@ function Contact() {
                     </svg>
                   </a>
                 </div>
-              </div>
             </div>
           </div>
 
